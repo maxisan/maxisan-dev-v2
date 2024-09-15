ad3296e740112415349b39336129e892188d2a40
Maximiliano Sanchez <maxisanchez19@gmail.com>
2024-09-15 19:27:56 +0100

    refactor: Update layout to include welcome message and navigation menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,9 +21,17 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${raleway.variable} ${openSans.variable} bg-black h-dvh py-6 px-6 relative sm:py-10 sm:px-10`}>
         <MainBackground />
-        <main className="h-full transition-all duration-300 rounded-3xl overflow-hidden bg-black">
-          {children}
-        </main>
+        <div className="grid grid-rows-[5rem_1fr_5rem] h-full transition-all duration-300 rounded-3xl overflow-hidden bg-black/80 md:grid-cols-[7rem_1fr] md:grid-rows-[5rem_1fr]">
+          <div className="text-white md:col-span-2">
+            Welcome message and social media links
+          </div>
+          <main className="md:col-start-2 row-start-2">
+            {children}
+          </main>
+          <header className="text-white md:col-start-1 md:row-start-2">
+            Navigation Menu
+          </header>
+        </div>
       </body>
     </html>
   );
